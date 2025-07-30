ae496696b276a446c102d6bfac5c9ff457df4212
Behruz <baha2323mers84@gmail.com>
2025-07-30 17:56:18 +0300

    Update admin.py

--- backend/src/recipes/admin.py
+++ backend/src/recipes/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
-from django.utils.safestring import mark_safe
 
 from constants import ADMIN_PIC_DOTS
 from recipes.models import (
